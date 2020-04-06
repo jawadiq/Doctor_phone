643edba457d6ec68c6704f32c9633524fdc66d7e
jawadiq <jawadiqbal84@gmail.com>
2020-04-06 14:38:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dell/doctor_phone/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dell.doctor_phone;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    MediaPlayer mediaPlayer;
+    SensorManager sensorManager;
+    Sensor sensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager (getSystemService())
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
